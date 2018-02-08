d25a7b7565e58a5f2d874764e1a5f6d1295c145b
jlnavarro42 <leandro.navarro42@hotmail.com>
2018-02-08 10:41:59 -0500

    fixed the post function. also added html page so user can upload a file without any external program

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var bodyParser = require('body-parser');
 var PNF = require('google-libphonenumber').PhoneNumberFormat;
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 const port = 3000;
+//needed to upload and open files
+var fs = require('fs'); //for opening files
+//for uploading
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 function cleanString(str){
 	return str.replace(/%20/g, " ");
@@ -35,7 +40,7 @@ app.get('/api/phonenumbers/parse/text/*', function(req,res){
 		res.status(200).json(jsonPhoneNumber);
 		
 		
-		
+
 		console.log(formatPhoneNumber);
 	}
 	catch(err){
@@ -45,17 +50,34 @@ app.get('/api/phonenumbers/parse/text/*', function(req,res){
 	
 });
 
-app.post('/api/phonenumbers/parse/file', function(req,res){
-	var str = req.body.phoneNumber;
-	var phoneNumber = phoneUtil.parse(str, 'US');
-	var formatPhoneNumber = phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
-	
-	var jsonPhoneNumber = {
-			"phoneNumber": formatPhoneNumber,
+app.get('/api/phonenumbers/parse/file', (req,res) =>{
+	res.sendFile(__dirname + "/post.html");
+});
+
+app.post('/api/phonenumbers/parse/file', upload.single('file'), function(req,res){
+	var result = [];
+	fs.readFile(req.file.path, (err, data)=>{
+		if(err){
+			res.status(400).send("No file found");
 		}
-	
-	res.status(200).json(jsonPhoneNumber);
-	console.log(formatPhoneNumber);
+		else{
+			//assuming the uploaded file is in regular text and not encoded in base64
+			var buf = Buffer.from(data, 'base64').toString();
+			var phoneNumbersArray = buf.split('\n');
+			for(var i = 0; i < phoneNumbersArray.length; i++){
+				try{
+					var phoneNumber = phoneUtil.parse(phoneNumbersArray[i], 'US');	
+					var formatPhoneNumber = phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+					result.push(formatPhoneNumber);		
+				}
+				catch(error){
+				
+				}
+			}
+			console.log(result);
+			res.status(200).json(result);
+		}
+	});
 });
 
 app.listen(port);
